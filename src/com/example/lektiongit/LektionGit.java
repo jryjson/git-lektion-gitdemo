7283c7cb4fe27fab0d4e9457b22e0cbecced7d1f
Jerry Johansson <info@jerryjohansson.com>
2019-09-17 13:48:27 +0200

    New line

--- src/com/example/lektiongit/LektionGit.java
+++ src/com/example/lektiongit/LektionGit.java
@@ -3,5 +3,6 @@ package com.example.lektiongit;
 public class LektionGit {
   public static void main(String[] args){
     System.out.println("Hello world");
+    System.out.println("Hello goodbye");
   }
 }
