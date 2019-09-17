3979b753867023db2be3c27f6412dca9d95634a8
Jerry Johansson <info@jerryjohansson.com>
2019-09-17 13:51:11 +0200

    Change Hello text

--- src/com/example/lektiongit/LektionGit.java
+++ src/com/example/lektiongit/LektionGit.java
@@ -2,7 +2,7 @@ package com.example.lektiongit;
 
 public class LektionGit {
   public static void main(String[] args){
-    System.out.println("Hello world");
+    System.out.println("Hello");
     System.out.println("Hello goodbye");
   }
 }
